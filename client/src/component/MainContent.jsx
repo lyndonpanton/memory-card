f47915aa33bb48eb90001a27daaf6bac02eb4586
Lyndon Panton <LyndonPanton@gmail.com>
2026-02-17 17:38:18 +0000

    Data: Retrieve memory card data from api

--- client/src/component/MainContent.jsx
+++ client/src/component/MainContent.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
+
 function MainContent() {
+    const [cardData, setCardData] = useState();
+    
+    // Should be re-rendered with component if player restarts game
+    useEffect(function() {
+        // Generate random number for type (0 - 18)
+        const TYPE_ID = Math.floor(Math.random() * 19);
+
+        fetch("https://pokeapi.co/api/v2/type/" + TYPE_ID)
+            .then(function (response) {
+                if (response.status === 200) {
+                    return response.json()
+                }
+            }).then(function (data) {
+                if (data !== null) {
+                    console.log(data.pokemon);
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+    }, []);
+
     return (
         <main>
             <h2>Pokémon Memory Card Game</h2>
 
-            <article>
-                ...cards
-            </article>
+            {/* How to play component */}
+
+            {/* Scoreboard component */}
+
+            {/* Card component */}
         </main>
     );
 }
