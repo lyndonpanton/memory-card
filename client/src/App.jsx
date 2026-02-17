467e810f52a8583e956b734f749933d8197e5610
Lyndon Panton <LyndonPanton@gmail.com>
2026-02-17 17:14:32 +0000

    Cotent: Add main content component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 
 import Header from "./component/Header.jsx";
+import MainContent from "./component/MainContent.jsx";
 
 function App() {
     const appName = "Memory Card Game";
 
     return (
-        <Header appName={ appName } />
+        <>
+            <Header appName={ appName } />
+            <MainContent />
+        </>
     );
 }
 
---
+++ client/src/component/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+    return (
+        <main>
+            <h2>Pokémon Memory Card Game</h2>
+
+            <article>
+                ...
+            </article>
+        </main>
+    );
+}
+
+export default MainContent;
