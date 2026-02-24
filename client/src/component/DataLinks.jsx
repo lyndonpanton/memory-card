632111976665f6af4de692529981909922cd0cf8
Lyndon Panton <LyndonPanton@gmail.com>
2026-02-24 13:22:34 +0000

    Asset: Import website icons

--- client/src/component/DataLinks.jsx
+++ client/src/component/DataLinks.jsx
@@ -3,15 +3,21 @@ import "../styles/DataLinks.css";
 function DataLinks(props) {
     return (
         <section className={ "data-links" }>
-            <ul>
+            <ul className={ "data-links-legal" }>
                 <li>Privacy Policy</li>
                 <li>Cookies</li>
                 <li>Terms And Conditions</li>
             </ul>
-            <ul>
-                <li>...Instagram</li>
-                <li>...Twitter</li>
-                <li>...Facebook</li>
+            <ul className={ "data-link-social" }>
+                <li>
+                    <img src={ "" } alt={ "" } />
+                </li>
+                <li>
+                    <img src={ "" } alt={ "" } />
+                </li>
+                <li>
+                    <img src={ "" } alt={ "" } />
+                </li>
             </ul>
         </section>
     )
